f7672bbb52e8a68f6fa2455a3cb254f0f5fe8d96
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-07-10 14:36:37 -0600

    Starwars, hello world

---
+++ exercises/Hello World/app.js
@@ -0,0 +1,14 @@
+var name = 'Hello Fred,'
+var age = 'how do you like being 27?'
+
+
+// var sayHello = function(){
+//     console.log("hello class")
+// }
+// sayHello()
+
+function sayHello(arg1){
+    console.log(name, age)
+    return(arg1)
+}
+sayHello()
