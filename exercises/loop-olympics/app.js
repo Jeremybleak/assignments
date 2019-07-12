a8af899db79e8dd3f88817ce619bd0f39528ead7
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-07-12 11:14:42 -0600

    day 4

---
+++ exercises/loop-olympics/app.js
@@ -0,0 +1,38 @@
+// preliminaries
+
+// for(i = 0; i < 10; i++){
+//     console.log(i)
+// }
+// for(i = 9; i > 0; i--){
+//     console.log(i)
+// }
+
+// var fruit = ["banana", "orange", "apple", "kiwi"]
+
+// for(i = 0; i < fruit.length; i++){
+//     console.log(fruit[i])
+// }
+
+// bronze
+
+var myArray = []
+
+for(i = 0; i < 10; i++){
+    myArray.push([i])
+}
+
+console.log(myArray)
+
+for(i = 0; i < 101; i++){
+    if(i % 2 === 0){
+    console.log(i)
+    }
+}
+
+var fruit = ["banana", "orange", "apple", "kiwi"]
+
+for(i = 0; i < fruit.length; i++){
+    console.log(fruit[i])
+}
+
+
