9530b8ef22f7de57a059e46c77c5c2eb62ef80b8
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-07-16 20:32:33 -0600

    Tues 16th july

---
+++ exercises/string-methods/app.js
@@ -0,0 +1,16 @@
+// function capilizeAndLowercase(str){
+//     let strUpr = str.toUpperCase()
+//     let strLwr = str.toLowerCase()
+//     return strUpr + strLwr
+// }
+// console.log(capilizeAndLowercase('Pizza'))
+
+// function halfLength(str1){
+//    let strHalf = str1.length / 2;
+//    return strHalf
+   
+// }
+// console.log(Math.floor(halfLength("jello")))
+
+
+   
