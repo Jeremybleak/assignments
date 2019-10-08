d7c0e4eeda738cb02b3fa8cbb34c6964c6337234
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-10-08 12:14:35 -0600

    css Flags practice

---
+++ exercises/arrayExercisesPrac/index.js
@@ -0,0 +1,12 @@
+
+var vegis = ['poop', 'Ethan', 'is', 'lettuce']
+var fruit = ['crap']
+
+const poppy = () => vegis.pop()
+const poppy2 = () => vegis.shift()
+const poppy3 = () => vegis.concat(fruit)
+poppy()
+poppy2()
+poppy3()
+
+console.log(vegis)
