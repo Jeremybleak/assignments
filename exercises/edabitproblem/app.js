0b45bd01f1648db856a333ac0405e1def36920c3
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-12-21 02:21:43 -0700

    jeremy sucks

--- exercises/edabitproblem/app.js
+++ exercises/edabitproblem/app.js
@@ -1,3 +1,15 @@
 function dividesEvenly(a, b) {
 	return a % b === 0
 }
+
+function countWords(str) {	
+	return str.split(' ').length
+}
+
+function Go(num) {
+	let arr = []
+	for(i = 0; i < num; i ++){
+		arr.push('-')
+	}
+	return arr.join('')
+}
---
+++ projects/jeremysucks/src/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='hero-title-position'>
+                <h2 className='hero-title'>Jeremy Bleak</h2>
+                <h1><b className='hero-sucks'>SUCKS</b></h1>
+                <h3 className='hero-subtitle'>So bad</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
