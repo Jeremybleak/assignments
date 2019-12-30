23887ba973df121442a2fdf80ee9412a2b6b2df4
Jeremy Bleak <Jeremybleak99@gmail.com>
2019-12-30 15:20:57 -0700

    Deploy

--- projects/jeremysucks/src/App.js
+++ projects/jeremysucks/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './Hero'
 import './App.css'
+import Form from './Form'
 
 const App = () => {
     return (
         <div>
             <Hero/>
+            <Form/>
         </div>
     )
 }
---
+++ projects/jeremysucks/src/Form.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class Form extends React.Component{
+    constructor(){
+        super()
+        
+        this.state={
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <form action="">
+                    <input type="text"/>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <input type="text"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Form
