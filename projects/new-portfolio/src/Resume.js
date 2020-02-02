812dbce698b40a6e9324c83243d2bdc45fd941c2
Jeremy Bleak <Jeremybleak99@gmail.com>
2020-02-01 17:49:06 -0700

    resume skills

--- projects/new-portfolio/src/Resume.js
+++ projects/new-portfolio/src/Resume.js
@@ -25,39 +25,75 @@ const Resume = () => {
                         </div>
                             <div>
                                 <h3 className='resume-subtitle'>Experience</h3>
-                                <div>
-                                    <h4 className='resume-project-name'><u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>V School - Full Stack Internship</u> (Sep 2019 - Dec 2019)</h4>
-                                    <div className='resume-list'>
-                                        <li>- Assist students with all assigned course work / personal projects</li>
-                                        <li>- Track student progress as well as conduct biweekly technical challenges with each student</li>
-                                        <li>- Acted as first resource of help for students, to reteach a subject or help walk them through their code/functionality of their code.</li>
+                                <div className='resume-job-container'>
+                                    <div>
+                                        <h4 className='resume-project-name'><u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>V School - Full Stack Internship</u> (Sep 2019 - Dec 2019)</h4>
+                                        <div className='resume-list'>
+                                            <li>- Assist students with all assigned course work / personal projects</li>
+                                            <li>- Track student progress as well as conduct biweekly technical challenges with each student</li>
+                                            <li>- Acted as first resource of help for students, to reteach a subject or help walk them through their code/functionality of their code.</li>
+                                        </div>
+                                        <div>
+                                            <h4 className='resume-accomplishments'> <u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Accomplishments:</u> Graduated and was selected to be an assistant and mentor to new students</h4>
+                                        </div>
                                     </div>
                                     <div>
-                                        <h4 className='resume-accomplishments'> <u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Accomplishments:</u> Graduated and was selected to be an assistant and mentor to new students</h4>
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='resume-project-name'><u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Groviv - Production Assistant </u>(Sep 2018 - May 2019)</h4>
-                                    <div className='resume-list'>
-                                        <li>- Built and maintained Controlled Agricultural Environments by using and operating various power tools and lifts.</li>
-                                        <li>- Improved product functionality by working collaboratively with a team of 8 to problem solve pain points which lead to moving from a part-time temporary position to a full-time position.</li>
-                                        <li>- Assembled 25% of the carts for the Controlled Agricultural Environments</li>
-                                    </div>
-                                    <div>
-                                        <h4 className='resume-accomplishments'> <u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Accomplishments:</u> Promoted from the assembly line to the production team after a week</h4>
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='resume-project-name'><u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Iceberg - Employee/Manager</u> (Sep 2018 - May 2019)</h4>
-                                    <div className='resume-list'>
-                                        <li>- Managed a team of 5, delegating tasks in a fast paced environment.</li>
-                                        <li> - Worked as a cleaner, cashier, cook, and making shakes.</li>
+                                        <h4 className='resume-project-name'><u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Groviv - Production Assistant </u>(Sep 2018 - May 2019)</h4>
+                                        <div className='resume-list'>
+                                            <li>- Built and maintained Controlled Agricultural Environments by using and operating various power tools and lifts.</li>
+                                            <li>- Improved product functionality by working collaboratively with a team of 8 to problem solve pain points which lead to moving from a part-time temporary position to a full-time position.</li>
+                                            <li>- Assembled 25% of the carts for the Controlled Agricultural Environments</li>
+                                        </div>
+                                        <div>
+                                            <h4 className='resume-accomplishments'> <u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Accomplishments:</u> Promoted from the assembly line to the production team after a week</h4>
+                                        </div>
                                     </div>
                                     <div>
-                                        <h4 className='resume-accomplishments'> <u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Accomplishments:</u> Promoted to manager in 6 months</h4>
+                                        <h4 className='resume-project-name'><u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Iceberg - Employee/Manager</u> (Sep 2018 - May 2019)</h4>
+                                        <div className='resume-list'>
+                                            <li>- Managed a team of 5, delegating tasks in a fast paced environment.</li>
+                                            <li> - Worked as a cleaner, cashier, cook, and making shakes.</li>
+                                        </div>
+                                        <div>
+                                            <h4 className='resume-accomplishments'> <u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Accomplishments:</u> Promoted to manager in 6 months</h4>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                        <div>
+                            <h3 className='resume-subtitle'>Skills</h3>
+                            <div className='resume-skills-container'>
+                                <div>
+                                    <h4 className='resume-project-name'><u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Technical </u>(Hard)</h4>
+                                    <div className='skill-container'>
+                                        <div className='skill'><h4>JavaScript</h4></div>
+                                        <div className='skill'><h4>React</h4></div>
+                                        <div className='skill'><h4>HTML</h4></div>
+                                        <div className='skill'><h4>CSS</h4></div>
+                                        <div className='skill'><h4>ES6</h4></div>
+                                        <div className='skill'><h4>Node.js</h4></div>
+                                        <div className='skill'><h4>Express</h4></div>
+                                        <div className='skill'><h4>AJAX/HTTP</h4></div>
+                                        <div className='skill'><h4>Mongoose</h4></div>
+                                        <div className='skill'><h4>MongoDB</h4></div>
+                                        <div className='skill'><h4>JSON</h4></div>
+                                        <div className='skill'><h4>GIT</h4></div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <h4 className='resume-project-name'><u style={{color: "#FFFFFF", textDecoration: 'none', fontFamily: 'heebo-extra-bold'}}>Interpersonal </u>(Soft)</h4>
+                                    <div className='skill-container'>
+                                        <div className='skill'><h4>Problem Solving</h4></div>
+                                        <div className='skill'><h4>Customer Service</h4></div>
+                                        <div className='skill'><h4>Management</h4></div>
+                                        <div className='skill'><h4>Fast Learner</h4></div>
+                                        <div className='skill'><h4>Time Management</h4></div>
+                                        <div className='skill'><h4>Communication</h4></div>
+                                        <div className='skill'><h4>Collaboration</h4></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>    
                     </div>
                 </div>
             </div>
