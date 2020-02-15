ace9d12055a67de7cafead3a9703ffe5d04f101a
Jeremy Bleak <Jeremybleak99@gmail.com>
2020-02-15 15:49:39 -0700

    Fixing user exp for Contact form

--- projects/new-portfolio/src/Contact.js
+++ projects/new-portfolio/src/Contact.js
@@ -7,8 +7,11 @@ class Contact extends Component {
 
         this.state={
             name: '',
+            nameColor: '',
             email: '',
+            emailColor: '',
             message: '',
+            messageColor: '',
             status: ''
         }
     }
@@ -26,8 +29,36 @@ class Contact extends Component {
         }
         e.preventDefault()
         if(this.state.name === '' || this.state.email === '' || this.state.message === ''){
+            if(this.state.name === ''){
+                this.setState({
+                    nameColor: 'red'
+                })
+            }else{
+                this.setState({
+                    nameColor: ''
+                })
+            }
+            if(this.state.email === ''){
+                this.setState({
+                    emailColor: 'red'
+                })
+            } else{
+                this.setState({
+                    emailColor: ''
+                })
+            }
+            if(this.state.message === ''){
+                this.setState({
+                    messageColor: 'red'
+                })
+            }else{
+                this.setState({
+                    messageColor: ''
+                })
+            }
+            console.log(this.state.nameColor)
             this.setState({
-                status: 'PLEASE FILL ALL THE PARAMATERS'
+                status: "OOPS! YOU'RE MISSING SOMETHING"
             })
             
         } else{
@@ -42,8 +73,11 @@ class Contact extends Component {
             });
             this.setState({
                 name: '',
+                nameColor: '',
                 email: '',
-                message: ''
+                emailColor: '',
+                message: '',
+                messageColor: ''
             })
 
         }
@@ -56,12 +90,12 @@ class Contact extends Component {
                         <h1 className='contact-title'>Get In Touch</h1>
                         <div>
                             <form className='form-alignment' action="" onSubmit={this.handleSubmit}>
-                                <input className='input-text' onChange={this.handleChange} value={this.state.name} name='name' type="text" placeholder='Name' />
-                                <input className='input-text'  onChange={this.handleChange} value={this.state.email} name='email' type="email" placeholder='Email' />
-                                <textarea placeholder='Message'  onChange={this.handleChange} value={this.state.message} name='message' className='input-type' rows="4" cols="50">
+                                <input className='input-text' style={{borderColor:this.state.nameColor}} onChange={this.handleChange} value={this.state.name} name='name' type="text" placeholder='Name' />
+                                <input className='input-text' style={{borderColor: this.state.emailColor}}  onChange={this.handleChange} value={this.state.email} name='email' type="email" placeholder='Email' />
+                                <textarea placeholder='Message' style={{borderColor: this.state.messageColor}}  onChange={this.handleChange} value={this.state.message} name='message' className='input-type' rows="4" cols="50">
                                 </textarea>
                                 <button className='form-button'>SUBMIT</button>
-                                <h3 className='status' style={ this.state.status==='SENT!'?{color:'green'} : {color:'red'}}>{this.state.status}</h3>
+                                <h3 className='status' style={ this.state.status==='SENT!'?{color:'#4072FF'} : {color:'red'}}>{this.state.status}</h3>
                             </form>
                         </div>
                     </div>
