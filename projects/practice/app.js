bfef19143855501cf01706e35fb97440451bc3aa
Jeremy Bleak <Jeremybleak99@gmail.com>
2020-02-19 18:52:05 -0700

    portfolio

---
+++ projects/practice/app.js
@@ -0,0 +1,16 @@
+function logthis(){
+    return [] == []
+}
+
+console.log(logthis())
+
+sum(10,20)
+diff(10,20)
+function sum(x,y){
+    return x + y
+}
+let diff = function(x,y){
+    return x - y
+}
+
+args
