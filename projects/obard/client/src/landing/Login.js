1b9e7fbd82f727283f0a8d5b24aecefdcfb06fdd
Jeremy Bleak <Jeremybleak99@gmail.com>
2021-02-05 18:06:04 -0700

    css for landing page login form

--- projects/obard/client/src/landing/Login.js
+++ projects/obard/client/src/landing/Login.js
@@ -1,13 +1,13 @@
 import React from 'react'
+//css
+import "./landingCSS/landing.css"
 
 export default function Login() {
         return (
             <div>
-                <form action="">
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
+                <form className="login-form" action="">
+                    <input className='login-username-input' type="text"/>
+                    <input type="password"/>
                     <button>login</button>
                 </form>
             </div>
