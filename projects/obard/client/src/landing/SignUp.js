1f07b2aafe3ebe98409eb65857aa26e545f7c517
Jeremy Bleak <Jeremybleak99@gmail.com>
2021-02-08 19:34:05 -0700

    adding css for sign up page

--- projects/obard/client/src/landing/SignUp.js
+++ projects/obard/client/src/landing/SignUp.js
@@ -3,13 +3,16 @@ import React from 'react'
 export default function SignUp() {
         return (
             <div>
-                <form action="">
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <button>signup</button>
-                </form>
-            </div>
+            <h2 className="returning-customers">Returning Customers</h2>
+            <form className="login-form" action="">
+                <h3>Username</h3>
+                <input className='login-username-input' type="text"/>
+                <h3>Password</h3>
+                <input className='login-password-input' type="password"/>
+                <h3>Confirm Password</h3>
+                <input className='login-password-input' type="password"/>
+                <button className="login-button">Create an Account</button>
+            </form>
+        </div>
         )
 }
