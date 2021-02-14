b36e80d0cc2336caf9633bae85bdf564b59dec3f
Jeremy Bleak <Jeremybleak99@gmail.com>
2021-02-13 22:15:36 -0700

    coding challenge for better interview

---
+++ exercises/codilityTest/app.js
@@ -0,0 +1,14 @@
+let solution = (S,K) => {
+    let howManyWeeks = K/7
+    const week = ['Mon','Tue','Wed','Thur','Fri','Sat','Sun']
+    for(i=0; i < howManyWeeks; i++){
+        week.push('Mon','Tue','Wed','Thur','Fri','Sat','Sun')
+    }
+    let dayOfWeek = week.indexOf(S)
+    let newDay = dayOfWeek + K
+    return week[newDay]
+}
+
+
+
+
