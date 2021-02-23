d58359005440a22488e55cc2ba499b259ead6388
Jeremy Bleak <Jeremybleak99@gmail.com>
2021-02-22 18:15:14 -0700

    testing out our obard code in my test repo

--- projects/obard/client/src/App.js
+++ projects/obard/client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Landing from './landing/Landing'
+import Home from './User.js/Home/Home'
 
 const App = () => {
     return(
         <div>
             <Landing/>
+            <Home />
         </div>
     )
 }
